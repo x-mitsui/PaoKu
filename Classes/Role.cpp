7752a34b407a2bf2f1adab67c04577e522e8f551
HeyUComeOn <x_mitsui@163.com>
2014-09-06 23:41:09 +0800

    第三次：碰撞检测

--- Classes/Role.cpp
+++ Classes/Role.cpp
@@ -28,7 +28,7 @@ void Role::extraInit()
 	mSprite->setPosition(x,y);
 	mSprite->setAnchorPoint(Vec2(0,0));
 
-	//必须添加在地图中，因为以后的setMapPointByRole()中的行为在地图中判断
+	//必须添加在地图中，因为以后的setMapPointByRole()中的行为在地图中判断,x的判断必须在同一系统中
 	getGameMap()->addChild(	mSprite, ROLE_ZORDER );
 
 	this->setCurrentRoleState(ROLE_STATE::ROLE_RUN_STATE);
@@ -80,6 +80,30 @@ void Role::runLogic()
 
 	*/
 
+	/*
+	
+		1.先判断向右方向的运动（注意先后逻辑顺序）
+		2.再判断向下方向的运动
+	
+	*/
+
+	if (isCollWithCollArea(COLL_STATE::COLL_RIGHT))
+	{
+		//右侧与墙碰撞就停止
+		this->setCurrentRoleState(ROLE_STATE::ROLE_STAND_BY);
+
+		Dlog::showLog("push the wall");
+		return;
+	}
+
+	if (!isCollWithCollArea(COLL_STATE::COLL_BOTTOM))
+	{
+		//如果不与地面接触
+		this->setCurrentRoleState(ROLE_STATE::ROLE_JUMP_DOWN_STATE);
+
+		Dlog::showLog("Drop");
+		return;
+	}
 	Vec2 pos = getCurrentRolePoint();
 	Vec2 curPos = pos + Vec2(xSpeed,0);
 	setCurrentRolePoint(curPos);
@@ -133,20 +157,19 @@ void Role::setMapPointByRole()
 
 	auto rolePoint = mSprite->getPosition();
 
-	/////////////////////////////////////////////////////////////////////////
+	///////////////////////确定x为地图系统节点中的坐标//////////////////////////////////////////////////
 	float x = rolePoint.x > visibleSize.width/4 ? rolePoint.x : visibleSize.width/4;
 
-	x = x < (mapSize.width - visibleSize.width*.8) ?x:(mapSize.width - visibleSize.width*.8);
+	x = x < (mapSize.width - visibleSize.width*.85) ? x:(mapSize.width - visibleSize.width*.85);
 
 	float y =rolePoint.y >visibleSize.height/3? rolePoint.y:visibleSize.height/3;
 
-	y = y < mapSize.height/2 ? y:mapSize.height/2;
+	y = y < mapSize.height/2 ? y : mapSize.height/2;
 	///////////////////////////////////////////////////////////////////////
 
 	Vec2 desPoint = Vec2(x,y);//目标点
-	Vec2 centerPoint = Vec2(visibleSize.width/5,visibleSize.height/3);//????????????
-
-	auto rltPoint = centerPoint - desPoint ;
+	Vec2 centerPoint = Vec2(visibleSize.width/4,visibleSize.height/3);//这是一个固定点；对于镜头来说，可以看成锚点，
+	auto rltPoint = centerPoint - desPoint ;//地图反方向运行
 
 	getGameMap()->setPosition(rltPoint);
 }
@@ -154,4 +177,74 @@ void Role::setMapPointByRole()
 void Role::camera_update(float delta)
 {
 	setMapPointByRole();
+}
+
+bool Role::isCollWithCollArea(COLL_STATE collState)
+{
+	bool Flag = false;
+
+	auto collGroup = getGameMap()->getObjectGroup("coll");
+	for (auto obj : collGroup->getObjects())
+	{
+		auto dict =  obj.asValueMap();//不要用“强制转换”来转换，要用这种形式转换
+		float x = dict["x"].asFloat();
+		float y = dict["y"].asFloat();
+
+		float width = dict["width"].asFloat();
+		float height = dict["height"].asFloat();
+
+		Rect box = Rect(x,y,width,height);
+
+		switch (collState)
+		{
+		case COLL_STATE::COLL_INVALID_STATE:
+			break;
+		case COLL_STATE::COLL_TOP:
+			Flag = isCollisionWithTop(box);
+			break;
+		case COLL_STATE::COLL_BOTTOM:
+			Flag = isCollisionWithBottom(box);
+			break;
+		case COLL_STATE::COLL_LEFT:
+			Flag = isCollisionWithLeft(box);
+			break;
+		case COLL_STATE::COLL_RIGHT:
+			Flag = isCollisionWithRight(box);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	return Flag;
+}
+
+bool Role::isCollisionWithLeft(Rect Box)
+{
+	//获取人物图片左边框中间点
+
+	auto roleBox = mSprite->getBoundingBox();
+	auto rolePoint = Vec2(roleBox.getMinX(),roleBox.getMidY());
+	return Box.containsPoint(rolePoint);
+}
+
+bool Role::isCollisionWithRight(Rect Box)
+{
+	auto roleBox = mSprite->getBoundingBox();
+	auto rolePoint = Vec2(roleBox.getMaxX(),roleBox.getMidY());
+	return Box.containsPoint(rolePoint);
+}
+
+bool Role::isCollisionWithTop(Rect Box)
+{
+	auto roleBox = mSprite->getBoundingBox();
+	auto rolePoint = Vec2(roleBox.getMidX(),roleBox.getMaxY());
+	return Box.containsPoint(rolePoint);
+}
+
+bool Role::isCollisionWithBottom(Rect Box)
+{
+	auto roleBox = mSprite->getBoundingBox();
+	auto rolePoint = Vec2(roleBox.getMidX(),roleBox.getMidY());
+	return Box.containsPoint(rolePoint);
 }
--- Classes/Role.h
+++ Classes/Role.h
@@ -12,6 +12,17 @@ enum class ROLE_STATE{
 	ROLE_JUMP_DOWN_STATE,
 	ROLE_STAND_BY
 };
+
+//用于判断碰撞方向的枚举类
+enum class COLL_STATE
+{
+	COLL_INVALID_STATE = 0,
+	COLL_TOP,
+	COLL_BOTTOM,
+	COLL_LEFT,
+	COLL_RIGHT
+};
+
 class Role :public cocos2d::Node
 {
 public:
@@ -40,6 +51,14 @@ private:
 	//根据人物的位置点设置镜头
 	void setMapPointByRole();
 	void camera_update(float delta);
+private:
+	//碰撞区检测函数
+	bool isCollWithCollArea(COLL_STATE collState);
+	bool isCollisionWithLeft(cocos2d::Rect Box);
+	bool isCollisionWithRight(cocos2d::Rect Box);
+	bool isCollisionWithTop(cocos2d::Rect Box);
+	bool isCollisionWithBottom(cocos2d::Rect Box);
+
 };
 
 
